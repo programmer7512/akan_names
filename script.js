c4028ba77140bf6881ca429e0afd9b52b2b52ae1
blackstar <blacknet1995@gmail.com>
2019-11-30 19:59:34 +0300

    array for male and female names

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function input() {
     var dd = parseInt(day);
     var year = document.getElementById("year").value;
     var yy = parseInt(year);
-    var cc = (yy - 1) / 100 + 1;
+    var cc = (yy-1) / 100+1;
     var result = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
     var boyNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var girlNames = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
